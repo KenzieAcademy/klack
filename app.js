b4c7561d449f47278c02a6a3e5f2688cc95d1e7d
Mike Cammarano <mike@kenzie.academy>
2018-04-18 11:37:40 -0700

    adding comments, for real this time

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ let users = {}
 app.use(express.static("./public"))
 app.use(express.json())
 
+// generic comparison function for case-insensitive alphabetic sorting on the name field
 function userSortFn(a, b) {
     var nameA = a.name.toUpperCase(); // ignore upper and lowercase
     var nameB = b.name.toUpperCase(); // ignore upper and lowercase
@@ -27,21 +28,38 @@ function userSortFn(a, b) {
 }
 
 app.get("/messages", (request, response) => {
+    // get the current time
     const now = Date.now();
-    const requireActiveSince = now - (15*1000) // consider inactive after 15 seconds
+
+    // consider users active if they have connected (GET or POST) in last 15 seconds
+    const requireActiveSince = now - (15*1000)
+
+    // create a new list of users with a flag indicating whether they have been active recently
     usersSimple = Object.keys(users).map((x) => ({name: x, active: (users[x] > requireActiveSince)}))
+
+    // sort the list of users alphabetically by name
     usersSimple.sort(userSortFn);
     usersSimple.filter((a) => (a.name !== request.query.for))
+
+    // update the requesting user's last access time
     users[request.query.for] = now;
+
+    // send the latest 40 messages and the full user list, annotated with active flags
     response.send({messages: messages.slice(-40), users: usersSimple})
 })
 
 app.post("/messages", (request, response) => {
     // add a timestamp to each incoming message.
-    let timestamp = Date.now()
+    const timestamp = Date.now()
     request.body.timestamp = timestamp
+
+    // append the new message to the message list
     messages.push(request.body)
+
+    // update the posting user's last access timestamp (so we know they are active)
     users[request.body.sender] = timestamp
+
+    // Send back the successful response.
     response.status(201)
     response.send(request.body)
 })
--- public/main.js
+++ public/main.js
@@ -3,17 +3,21 @@ const messagesDiv = document.getElementById("messageslist");
 const textarea = document.getElementById("newmessage");
 const ding = new Audio('typewriter_ding.m4a');
 
+// this will be the list of all messages displayed on the client
 let messages = [{timestamp: 0}];
 
 let name = window.prompt("Enter your name");
+// if they didn't type anything at the prompt, make up a random name
 if(name.length===0) name = "Anon-" + Math.floor(Math.random()*1000);
 
+// add the sender and text of one new message to the bottom of the message list
 function appendMessage(msg) {
     messages.push(msg);
     messagesDiv.innerHTML +=
       `<div class="message"><strong>${msg.sender}</strong><br>${msg.message}</div>`;
 }
 
+// redraw the entire list of users, indicating active/inactive
 function listUsers(users) {
     let userStrings = users.map((user) =>
         (user.active ? `<span class="active"><span class="cyan">&#9679;</span> ${user.name}</span>` : `<span class="inactive">&#9675; ${user.name}</span>`)
@@ -21,9 +25,12 @@ function listUsers(users) {
     userList.innerHTML = userStrings.join("<br>");
 }
 
+// true if the messages div is already scrolled down to the latest message
 function scrolledToBottom() {
     return messagesDiv.scrollTop + 600 >= messagesDiv.scrollHeight;
 }
+
+// force the messages div to scroll to the latest message
 function scrollMessages() {
     messagesDiv.scrollTop = messagesDiv.scrollHeight;
 }
@@ -32,9 +39,14 @@ function fetchMessages() {
     fetch("/messages?for=" + encodeURIComponent(name))
         .then(response => response.json())
         .then(data => {
+            // if already scrolled to bottom, do so again after adding messages
             const shouldScroll = scrolledToBottom();
             var shouldDing = false;
+
+            // redraw the user list
             listUsers(data.users);
+
+            // examine all received messages, add those newer than the last one shown
             for(let i = 0; i < data.messages.length; i++){ 
                 let msg = data.messages[i];
                 if(msg.timestamp > messages[messages.length-1].timestamp) {
@@ -44,11 +56,14 @@ function fetchMessages() {
             }
             if(shouldScroll && shouldDing) scrollMessages();
             if(shouldDing) ding.play();
+
+            // poll again after waiting 5 seconds
             setTimeout(fetchMessages, 5000);
         })
 }
 
 document.getElementById("newmessage").addEventListener("keypress", (event) => {
+    // if the key pressed was enter (and not shift enter), post the message.
     if(event.keyCode === 13 && !event.shiftKey) {
         textarea.disabled = true;
         const postRequestOptions = {
@@ -63,6 +78,8 @@ document.getElementById("newmessage").addEventListener("keypress", (event) => {
             .then(msg => {
                 appendMessage(msg);
                 scrollMessages();
+
+                // reset the textarea
                 textarea.value="";
                 textarea.disabled = false;
                 textarea.focus();
@@ -70,4 +87,5 @@ document.getElementById("newmessage").addEventListener("keypress", (event) => {
     }
 })
 
+// call on startup to populate the messages and start the polling loop
 fetchMessages();
